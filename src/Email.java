8e3c1ca8f8e2dbef4f20ecdb5ca1aa2d5c68d3a1
kpilszak <klaudia.pilszak@gmail.com>
2020-01-30 19:18:39 +0100

    Generate a random password

--- src/Email.java
+++ src/Email.java
@@ -6,6 +6,7 @@ public class Email {
     private String password;
     private String department;
     private int mailboxCapacity;
+    private int defaultPasswordLength = 10;
     private String alternateEmail;
 
     public Email(String firstName, String lastName) {
@@ -15,6 +16,9 @@ public class Email {
 
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
+
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is: " + this.password);
     }
 
     private String setDepartment() {
@@ -30,5 +34,15 @@ public class Email {
         } else {
             return "";
         }
+    }
+
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGIJKLMNOPQRSTUWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
     }
 }
